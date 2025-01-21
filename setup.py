89183ac7dfb5b988d6cd16ef2dbf384ebcee5c49
“chris <christianwagner03@gmail.com>
2025-01-21 12:38:41 +0100

    fix setup2

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from distutils.core import setup
+from setuptools import setup
 setup(name='sucrack',
       version='1.0',
       py_modules=['sucrack'],
       install_requires=['pexpect'],
+      entry_points={"console_scripts": ["sucrack=sucrack.main:main"]},
       )
