abf6b4f7ef84426535f9967b0f8ca0d2e9870539
“chris <christianwagner03@gmail.com>
2025-01-21 12:48:15 +0100

    fix setup4

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup
-setup(name='sucrack',
-      version='1.0',
-      py_modules=['sucrack'],
-      install_requires=['pexpect'],
-      entry_points={"console_scripts": ["sucrack=sucrack.main:main"]},
+setup(
+        name='sucrack',
+        version='1.0',
+        install_requires=['pexpect'],
+        entry_points={"console_scripts": ["sucrack=sucrack.main:main"]},
+        url="https://github.com/winter1245/sucrack",
       )
