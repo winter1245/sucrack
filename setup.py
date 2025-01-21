9b4b8f788f6fcfcf3b02b75bdc37a8747ee98cd4
winter1245 <149313933+winter1245@users.noreply.github.com>
2025-01-21 12:02:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from distutils.core import setup
 setup(name='sucrack',
       version='1.0',
-      py_modules=['sucrack.py'],
+      py_modules=['sucrack'],
+      install_requires=['pexpect'],
       )
